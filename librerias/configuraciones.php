3942d710c8f4b683ca31c025e108067ca2056ae9
Dprado28 <85330590+Dprado28@users.noreply.github.com>
2021-06-04 00:01:17 -0500

    Add files via upload

---
+++ librerias/configuraciones.php
@@ -0,0 +1,13 @@
+<?php
+session_name("NOMBRE_DE_LA_EMPRESA");
+session_start();
+
+const IP_MAQUINA="localhost";
+const BASE_DE_DATOS="veterinaria";
+
+const USUARIO_ADMINISTRADOR="postgres";
+const CLAVE_ADMINISTRADOR="123";
+
+const USUARIO_LIMITADO="";
+const CLAVE_LIMITADO="";
+?>
